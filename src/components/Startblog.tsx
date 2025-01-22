3686c3a5156951b1c78643fc33eac0c1c1cf1c16
Vaibhav Singh <24b2725@iitb.ac.in>
2025-01-22 23:18:54 +0530

    added startblog

---
+++ src/components/Startblog.tsx
@@ -0,0 +1,18 @@
+function Startblog() {
+  return (
+    <>
+      <div className="w-[723px] h-[200px] bg-[#1B1826] rounded-[18px] flex flex-col items-start justify-center px-4">
+        <span className="font-lexend text-2xl font-medium leading-10  text-[#D6C9F8] decoration-skip-ink mb-4">
+          <p>Hi Rakshit! What's on your mind today?</p>
+        </span>
+        <img
+          src="./src/assets/img.png"
+          alt="Logo"
+          className="h-12 w-25 rounded-[24px] self-start"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Startblog;
