75b6d67ce5d3e8c1b302a9696d96f3b89655859b
Rakshit-Sawarn-iitb <rakshitsawarn21@gmail.com>
2025-02-04 10:12:20 +0530

    Issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/register'; // Adjust path as needed
-import { profile } from "./assets/dummy";
-import BlogCard from "./components/BlogCard";
-import PeopleYouMayKnow from "./components/peopleYouMayKnow";
-import ProfilePage from "./components/profilePage";
-import ConnectButton from "./components/shared/connectButton";
-import Navbar from "./components/Navbar";
-import Logo from "./components/Shared/Logo.tsx";
-import Startblog from "./components/startblog.tsx";
-import TalkBlog from "./components/Talkblog.tsx";
-import Postbutton from "./components/Shared/Postbutton.tsx";
-import Trendingtopics from "./components/Trendingtopics.tsx";
+// import { profile } from "./assets/dummy";
+// import BlogCard from "./components/BlogCard";
+// import PeopleYouMayKnow from "./components/peopleYouMayKnow";
+// import ProfilePage from "./components/profilePage";
+// import ConnectButton from "./components/shared/connectButton";
+// import Navbar from "./components/Navbar";
+// import Logo from "./components/Shared/Logo.tsx";
+// import Startblog from "./components/startblog.tsx";
+// import TalkBlog from "./components/Talkblog.tsx";
+// import Postbutton from "./components/Shared/Postbutton.tsx";
+// import Trendingtopics from "./components/Trendingtopics.tsx";
 
 function App() {
   return (
@@ -21,5 +21,6 @@ function App() {
         {/* You can add other routes as needed */}
       </Routes>
     </Router>
-
+  );
+}
 export default App;
