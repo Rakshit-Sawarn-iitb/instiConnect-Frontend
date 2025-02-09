9232164e7f4f6ef5b20056ba5390b46d2120ff58
Rahul <palr92933@gmail.com>
2025-02-09 14:12:02 +0530

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/register'; // Adjust path as needed
-<<<<<<< HEAD
-import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 import Login from './components/login';
 
-=======
-import { profile } from "./assets/dummy";
-import BlogCard from "./components/BlogCard";
-import PeopleYouMayKnow from "./components/peopleYouMayKnow";
-import ProfilePage from "./components/profilePage";
-import ConnectButton from "./components/shared/connectButton";
-import Navbar from "./components/Navbar";
-import Logo from "./components/Shared/Logo.tsx";
-import Startblog from "./components/startblog.tsx";
->>>>>>> 24f547ad669b2493c90c2b33f32d106f6571e175
+// import { profile } from "./assets/dummy";
+// import BlogCard from "./components/BlogCard";
+// import PeopleYouMayKnow from "./components/peopleYouMayKnow";
+// import ProfilePage from "./components/profilePage";
+// import ConnectButton from "./components/shared/connectButton";
+// import Navbar from "./components/Navbar";
+// import Logo from "./components/Shared/Logo.tsx";
+// import Startblog from "./components/startblog.tsx";
+
 
 function App() {
   return (
@@ -27,6 +26,7 @@ function App() {
         {/* You can add other routes as needed */}
       </Routes>
     </Router>
-
+  )
+}
 
 export default App;
